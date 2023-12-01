864d83a36623cd22dba4b0d29b2f3fc6fff6d79a
Sahin Deniz <seahindeniz@gmail.com>
2023-12-01 21:18:44 +0300

    build: Update branches in .releaserc.cjs

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -1,6 +1,10 @@
 /** @type {import('semantic-release').GlobalConfig} */
 module.exports = {
-    branches: ['main'],
+    branches: [
+        'main',
+        { name: 'beta', prerelease: true },
+        { name: 'alpha', prerelease: true },
+    ],
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
