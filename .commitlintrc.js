7c62fa105d2294f0aa027adfaa7abb11401f26a7
Sahin Deniz <seahindeniz@gmail.com>
2023-12-10 15:08:13 +0300

    refactor: organize commitlintrc

---
+++ .commitlintrc.js
@@ -0,0 +1,7 @@
+/** @type {import('@commitlint/types').UserConfig} */
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'body-max-line-length': [0],
+    },
+}
