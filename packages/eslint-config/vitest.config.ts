3802993b429ab3b2c69df7d6e94b75195f47a7d0
Sahin Deniz <seahindeniz@gmail.com>
2023-12-24 21:33:03 +0300

    test: add vitest.config.ts for ESLint configuration

---
+++ packages/eslint-config/vitest.config.ts
@@ -0,0 +1,15 @@
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+    plugins: [tsconfigPaths()],
+    test: {
+        root: './tests',
+        watch: true,
+        coverage: {
+            provider: 'v8',
+            reporter: ['text', 'json', 'html'],
+            reportsDirectory: './coverage',
+        },
+    },
+});
