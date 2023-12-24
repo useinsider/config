feb3362c77eceb9b4a15b565e6aa0c539e14e38a
Sahin Deniz <seahindeniz@gmail.com>
2023-12-24 21:44:55 +0300

    chore(eslint-config): update ESLint configuration

--- packages/eslint-config/.eslintrc.cjs
+++ packages/eslint-config/.eslintrc.cjs
@@ -3,16 +3,27 @@ module.exports = {
     root: true,
     overrides: [
         {
-            files: ['./**/*.{cjs,js}'],
+            files: ['./src/**/*.{cjs,js}'],
             extends: [
                 require.resolve('./src/node/index.cjs'),
             ],
         },
         {
-            files: ['./*.{cjs,js}'],
+            files: ['./*.{cjs,js}', './tests/**/eslint*.*'],
             extends: [
+                require.resolve('./src/node/index.cjs'),
                 require.resolve('./src/config/index.cjs'),
             ],
+        },
+        {
+            files: ['./*.ts', 'tests/test-facility/*.ts', './tests/**/*.test.ts'],
+            extends: [
+                require.resolve('./src/typescript/index.cjs'),
+                require.resolve('./src/config/index.cjs'),
+            ],
+            parserOptions: {
+                project: ['./tsconfig.test.json'],
+            },
         },
     ],
 };
