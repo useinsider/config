86f59126d27633c131a6f9e4d5ba5b1922a02b63
Sahin Deniz <seahindeniz@gmail.com>
2024-03-25 09:54:48 +0300

    build: :arrow_up: upgrade dependencies

--- packages/eslint-config/src/utils/merge.ts
+++ packages/eslint-config/src/utils/merge.ts
@@ -1,4 +1,4 @@
-import tsMerge from 'ts-deepmerge';
+import { merge as tsMerge } from 'ts-deepmerge';
 
 export function merge<T extends NonNullable<unknown>[]> (...args: T): T[number] {
     return tsMerge.withOptions({ mergeArrays: false }, ...args) as unknown as T;
